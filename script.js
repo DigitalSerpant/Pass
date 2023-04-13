34c12af73d845167d8384520dc9c03c3b91f12fb
Digital-S <127454668+DigitalSerpant@users.noreply.github.com>
2023-04-12 17:13:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function sus() {
     // Check if the username and password match the expected values
     if (username === 'user' && password === '123456') {
       // If they match, open youtube.com in a new window
-      window.open('https://www.youtube.com/', '_blank');
+      window.open('https://sites.google.com/student.harmonytx.org/ourharmonyportal/page', '_blank');
     } else {
       // If they don't match, display an error message
       alert('Error: Invalid username or password');
